47909956551f170acf432759971dd6ee0524281b
Renan Oliveira <renandemelo@gmail.com>
2013-08-13 20:06:53 -0300

    corrigindo

--- web.rb
+++ web.rb
@@ -7,5 +7,5 @@ get "/valor-ferias" do
 	cal = CalculadoraCLT.new(params["salario"].to_f)
 	dias = params["dias"].to_i unless params["dias"].nil?
 	valor = cal.valor_ferias(:dias => dias)
-	"Valor das ferias:#{valor}"
+	"Ferias:#{valor}"
 end
