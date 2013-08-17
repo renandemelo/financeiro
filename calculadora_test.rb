8fa42cf75329ab7a86433e66fddb82f7d07c7546
Renan Oliveira <renandemelo@gmail.com>
2013-08-17 16:25:54 -0300

    implementando...

--- calculadora_test.rb
+++ calculadora_test.rb
@@ -3,4 +3,13 @@ require './calculadora'
 
 class CalculadoraTest < Test::Unit::TestCase
 
+
+	def test_soma_4_e_5_da_9
+		assert_equal(9,Calculadora.new.soma(4,5))
+	end 
+
+
+	def test_soma_20_e_10_da_30
+		assert_equal(30,Calculadora.new.soma(10,20))
+	end 
 end
