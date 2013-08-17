c47b002c4c7e9ac5f161a85ad419cab5150168d0
Renan Oliveira <renandemelo@gmail.com>
2013-08-17 11:36:26 -0300

    implementando...

--- calculadora_test.rb
+++ calculadora_test.rb
@@ -3,9 +3,12 @@ require './calculadora'
 
 class CalculadoraTest < Test::Unit::TestCase
 
-	def test_
-		
-		assert_equal()
+	def test_soma_3_e_4_eh_7		
+		assert_equal(7, Calculadora.new.soma(3,4))
+	end
+
+	def test_soma_10_e_20_eh_30		
+		assert_equal(30, Calculadora.new.soma(10,20))
 	end
 
 end
--- web.rb
+++ web.rb
@@ -4,7 +4,8 @@ require './calculadora'
 set :port, 3000
 
 get "/soma" do
-	a = params[:a]
-	b = params[:b]
-	"Soma de #{a} com #{b}"
+	a = params[:a].to_i
+	b = params[:b].to_i
+	soma = Calculadora.new.soma(a,b)
+	"Soma de #{a} com #{b} eh: #{soma}"
 end
