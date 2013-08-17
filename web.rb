17b2041978c3fbf3593dfa5ed37c67999dca1348
Renan Oliveira <renandemelo@gmail.com>
2013-08-17 03:23:30 -0300

    voltando

--- web.rb
+++ web.rb
@@ -7,5 +7,5 @@ get "/soma" do
 	a = params[:a]
 	b = params[:b]
 	resultado = Calculadora.new.soma(a,b)
-	"Soma de #{a} com #{b} eh: #{resultado}"
+	"Soma de #{a} com #{b} eh: #{resultado}!!!!!!"
 end
